14f2512a28c18dd8e8da3ee2a3aed850bfb0bbd1
Peter Dinis <peterdinis0@pobox.sk>
2024-02-12 18:41:31 +0100

    feat: Update app.controller with swagger

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("app")
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    @ApiOperation({
+        description: "Simple endpoint that return Hello World"
+    })
+    @ApiResponse({
+        status: 200
+    })
     @Get()
     getHello(): string {
         return this.appService.getHello();
