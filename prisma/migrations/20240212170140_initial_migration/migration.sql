75bda30540dddb723e2658b927a4fbc3160eb015
Peter Dinis <peterdinis0@pobox.sk>
2024-02-12 18:01:53 +0100

    infra: First migration

---
+++ prisma/migrations/20240212170140_initial_migration/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
