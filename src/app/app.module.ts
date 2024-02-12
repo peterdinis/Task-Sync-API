2083852890f7181d6e053825df860aa9849b3ef8
Peter Dinis <peterdinis0@pobox.sk>
2024-02-12 18:51:01 +0100

    feat: New users Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-    imports: [PrismaModule, AuthModule],
+    imports: [PrismaModule, AuthModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
