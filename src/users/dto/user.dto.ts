538fad4d67f51f6b0450129896e34e6dc77968bb
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 21:15:50 +0100

    feat: Update task module

---
+++ src/users/dto/user.dto.ts
@@ -0,0 +1,24 @@
+import {
+    IsEmail,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    MinLength,
+} from 'class-validator';
+
+export class BasicUserDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(4, {
+        message: 'Password must have more than 4 letters',
+    })
+    password: string;
+}
