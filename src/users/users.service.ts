8e25bd6c456f6e98b47a408d714a023bdde14038
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 17:18:22 +0100

    fix: Fixing small problem with prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-    create(createUserDto: CreateUserDto) {
-        return 'This action adds a new user';
-    }
+   constructor(private readonly prismaService: PrismaService) {}
 
-    findAll() {
-        return `This action returns all users`;
-    }
+   async findOne(id: string) {
+     const findOneUser = await this.prismaService.user.findUnique({
+      where: {
+        id
+      },
+      include: {
+        Task: true
+      }
+     })
 
-    findOne(id: string) {
-        return `This action returns a #${id} user`;
-    }
-
-    update(id: number, updateUserDto: UpdateUserDto) {
-        return `This action updates a #${id} user`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} user`;
-    }
+     
+   }
 }
