4e943cd7fb648d24d6cc9a42a024a45b5c627a70
Peter Dinis <peterdinis0@pobox.sk>
2024-02-13 16:57:19 +0100

    fix: Fixing small problem with jwt.strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,12 +1,15 @@
+
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import {PassportStrategy} from "@nestjs/passport";
-import {ExtractJwt, Strategy} from "paspport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-jwt";
 import { UsersService } from "src/users/users.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private configService: ConfigService, private userService: UsersService) {}
+    constructor(private configService: ConfigService, private userService: UsersService) {
+        
+    }
 
     
 }
