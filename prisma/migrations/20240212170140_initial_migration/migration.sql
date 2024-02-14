1afff81d0d4d6a0d9b1775f538571205c6e65eac
Peter Dinis <peterdinis0@pobox.sk>
2024-02-14 20:59:36 +0100

    infra: Postgresql migration WIP

--- prisma/migrations/20240212170140_initial_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "username" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
