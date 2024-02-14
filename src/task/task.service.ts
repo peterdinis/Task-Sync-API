4fcc29307f0e9e584ab2a753d489e7bc172db6fd
Peter Dinis <peterdinis0@pobox.sk>
2024-02-14 20:57:46 +0100

    FOO

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,26 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TaskService {
-    create(createTaskDto: CreateTaskDto) {
-        return 'This action adds a new task';
-    }
-
-    findAll() {
-        return `This action returns all task`;
-    }
-
-    findOne(id: number) {
-        return `This action returns a #${id} task`;
-    }
-
-    update(id: number, updateTaskDto: UpdateTaskDto) {
-        return `This action updates a #${id} task`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} task`;
-    }
+   constructor(private readonly prismaService: PrismaService) {}
 }
