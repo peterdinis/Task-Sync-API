a01858f195f3a62cda9606b3b2cfdefca85f76be
Peter Dinis <peterdinis0@pobox.sk>
2024-02-16 21:56:21 +0100

    feat: Start adding here new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from 'src/task/task.module';
 import { TaskBlockModule } from 'src/task-block/task-block.module';
 import { TaskTimerModule } from 'src/task-timer/task-timer.module';
+import { ProjectModule } from 'src/project/project.module';
 
 @Module({
     imports: [
         PrismaModule,
         AuthModule,
         UsersModule,
+        ProjectModule,
         ConfigModule.forRoot({
             isGlobal: true,
         }),
