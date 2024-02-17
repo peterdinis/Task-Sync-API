4b5e769350efb82ed09df984d46525e8ac7e0a71
Peter Dinis <peterdinis0@pobox.sk>
2024-02-17 10:43:18 +0100

    infra: Update swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
@@ -13,6 +13,7 @@ async function bootstrap() {
         .setTitle('Talk Sync')
         .setDescription('The talk sync api Documentation')
         .setVersion('1.0')
+        .addBearerAuth()
         .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
