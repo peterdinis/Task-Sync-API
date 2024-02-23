f9d153b7038e479e36ad25299466d1b1427a3558
Peter Dinis <peterdinis0@pobox.sk>
2024-02-23 21:11:20 +0100

    fix: Fixing profile endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,11 @@ import {
     Controller,
     Get,
     Param,
-    ParseBoolPipe,
     Post,
     Put,
     Req,
     Res,
     UnauthorizedException,
-    UseGuards,
     UsePipes,
     ValidationPipe,
 } from '@nestjs/common';
@@ -24,7 +22,6 @@ import { Response, Request } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
-import { JwtAuthGuard } from './guards/jwt.guard';
 import { UsersService } from 'src/users/users.service';
 import { UpdateUserDto } from './dto/update.dto';
 
@@ -128,7 +125,6 @@ export class AuthController {
         status: 200,
     })
     @ApiBearerAuth()
-    @UseGuards(new JwtAuthGuard('jwt'))
     @Get('/profile')
     async getProfile(@Req() req: Request) {
         const userId = req.user['id'];
